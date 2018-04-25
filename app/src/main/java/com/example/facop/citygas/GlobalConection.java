0adcad7f96afb6fcc7c5b94f9caaa2b5428a1327
Roberto Rodriguez <emmanuelr48@gmail.com>
2018-04-25 17:55:04 -0600

    Add CityGas

--- app/src/main/java/com/example/facop/citygas/GlobalConection.java
+++ app/src/main/java/com/example/facop/citygas/GlobalConection.java
@@ -6,7 +6,7 @@ package com.example.facop.citygas;
 
 public class GlobalConection {
     //static    String url = "https://citygasapi.000webhostapp.com/api/";
-    static    String url = "https://citygasapi.000webhostapp.com/api/";
+    static    String url = "https://192.168.1.18/gibran/api/";
 
     static  String geturl() {
         return url;
