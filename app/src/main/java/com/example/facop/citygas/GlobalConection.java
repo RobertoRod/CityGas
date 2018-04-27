c35e158026b3956d3437f23eb94cf501d083e9ff
Roberto Rodriguez <emmanuelr48@gmail.com>
2018-04-27 02:37:55 -0600

    Cambiando a ip publica

--- app/src/main/java/com/example/facop/citygas/GlobalConection.java
+++ app/src/main/java/com/example/facop/citygas/GlobalConection.java
@@ -5,8 +5,8 @@ package com.example.facop.citygas;
  */
 
 public class GlobalConection {
-    //static    String url = "https://citygasapi.000webhostapp.com/api/";
-    static    String url = "https://192.168.1.18/gibran/api/";
+    static    String url = "https://citygasapi.000webhostapp.com/api/";
+    //static    String url = "https://192.168.1.18/gibran/api/";
 
     static  String geturl() {
         return url;
